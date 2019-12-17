5c7e1bfb24d12b0e0c5a02848af757b771b65cd9
Jonathan V. Scotto <52258891+jonavsco@users.noreply.github.com>
2019-12-17 08:27:01 -0500

    Update UsoPractica.java
    desde github4

--- src/practicab/UsoPractica.java
+++ src/practicab/UsoPractica.java
@@ -6,7 +6,7 @@ public class UsoPractica {
     public static void main(String[] args) {
             Practica p = new Practica();
            //no se porque funciona
-           //
+           //probando 
     }
 
    
