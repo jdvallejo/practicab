0227e0850675afc73ddfc87312ce8334921de196
jonna <jonna@DESKTOP-AIDLV31>
2019-12-17 07:53:58 -0500

    Proyecto de practica

---
+++ src/practicab/UsoPractica.java
@@ -0,0 +1,14 @@
+
+package practicab;
+
+public class UsoPractica {
+    
+    public static void main(String[] args) {
+            Practica p = new Practica();
+           //no se porque funciona
+           //
+    }
+
+   
+    
+}
